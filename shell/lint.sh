c493a3c3bcb151c471078e50def6f4b23110e9fc
Ramesh Sampath <rameshsampath@google.com>
2023-09-12 07:09:20 -0700

    Rename `keras` to `tf_keras` in `shell/lint.sh`
    PiperOrigin-RevId: 564711233

--- shell/lint.sh
+++ shell/lint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-isort --check --sl -c keras
+isort --check --sl -c tf_keras
 if ! [ $? -eq 0 ]
 then
   echo "Please run \"sh shell/format.sh\" to format the code."
   exit 1
 fi
 echo "no issues with isort"
-flake8 keras
+flake8 tf_keras
 if ! [ $? -eq 0 ]
 then
   echo "Please fix the code style issue."
   exit 1
 fi
 echo "no issues with flake8"
-black --check --line-length 80 keras
+black --check --line-length 80 tf_keras
 if ! [ $? -eq 0 ]
 then
   echo "Please run \"sh shell/format.sh\" to format the code."
