d23071fc91df34afcc2290f88cdb3029677b661e
Ramesh Sampath <rameshsampath@google.com>
2023-10-13 10:42:43 -0700

    Align Python version with TensorFlow and `Keras` version to `2.16`
    PiperOrigin-RevId: 573260392

--- tf_keras/__init__.py
+++ tf_keras/__init__.py
@@ -31,6 +31,6 @@ from tf_keras.testing_infra import test_utils
 from tensorflow.python import tf2
 from tensorflow.python.util.tf_export import keras_export
 
-__version__ = "2.15.0"
+__version__ = "2.16.0"
 
 keras_export("keras.__version__").export_constant(__name__, "__version__")
