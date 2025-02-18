0e3c9339c0801fea70959bb1a2fb52a221e72279
Raviteja Gorijala <gorijala@google.com>
2025-02-18 11:18:40 -0800

    Remove pinning protobuf version since 3.20.3 is old and not supported by Tensorflow
    PiperOrigin-RevId: 728286886

--- tf_keras/kokoro/github/ubuntu/gpu/build.sh
+++ tf_keras/kokoro/github/ubuntu/gpu/build.sh
@@ -42,7 +42,7 @@ pip uninstall -y keras-nightly
 
 # LD Library Path needs to be same as TensorFlow Ubuntu Docker build -
 # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/tools/tf_sig_build_dockerfiles/
-export LD_LIBRARY_PATH="/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/tensorrt/lib"
+# export LD_LIBRARY_PATH="/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/tensorrt/lib"
 CUDA_TOOLKIT_PATH="/usr/local/cuda-11.8"
 TF_CUDA_CONFIG_REPO="@ubuntu20.04-gcc9_manylinux2014-cuda11.8-cudnn8.6-tensorrt8.4_config_cuda"
 TF_CUDA_COMPUTE_CAPABILITIES="sm_35,sm_50,sm_60,sm_70,sm_75,compute_80"
